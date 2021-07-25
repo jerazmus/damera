e6a35abc7a8b039891ef637e9e892c7881f8c4a7
Jakub Erazmus <47739109+jerazmus@users.noreply.github.com>
2021-07-25 18:06:22 +0200

    home revert

--- frontend/src/components/pages/Home.vue
+++ frontend/src/components/pages/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
+    <div id="cover">
     <p id="title">dam<span class="era">era</span>.</p>
     <Forms />
+    </div>
   </div>
 </template>
 
@@ -20,6 +22,14 @@ export default {
 .home {
   margin: 0;
   font-family: "Poppins", sans-serif;
+  padding: 0;
+  min-height: 100vh;
+  height: auto;
+  background: url("../../assets/phone.jpg");
+  background-position: -5% 0%;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: 40vw 110vh;
 }
 
 #title {
@@ -33,6 +43,17 @@ export default {
 
 .era {
   color: black;
+}
+
+#cover {
+  background-color: rgba(255, 255, 255, 1);
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 70vw;
+  position: absolute;
+  right: 0;
+  box-shadow: rgba(0, 0, 0, 0.6) 0px 5px 15px;
 }
 
 /* Extra small devices (phones, 600px and down) */
@@ -51,6 +72,14 @@ export default {
     width: 100vw;
     margin: 0;
     padding: 0;
+    background: none;
+  }
+
+  #cover {
+    height: 100vh;
+    width: 100vw;
+    box-shadow: none;
+    right: auto;
   }
 }
 
@@ -70,6 +99,14 @@ export default {
     width: 100vw;
     margin: 0;
     padding: 0;
+    background: none;
+  }
+
+  #cover {
+    height: 100vh;
+    width: 100vw;
+    box-shadow: none;
+    right: auto;
   }
 }
 
@@ -89,6 +126,14 @@ export default {
     width: 100vw;
     margin: 0;
     padding: 0;
+    background: none;
+  }
+
+  #cover {
+    height: 100vh;
+    width: 100vw;
+    box-shadow: none;
+    right: auto;
   }
 }
 
@@ -108,6 +153,14 @@ export default {
     width: 100vw;
     margin: 0;
     padding: 0;
+    background: none;
+  }
+
+  #cover {
+    height: 100vh;
+    width: 100vw;
+    box-shadow: none;
+    right: auto;
   }
 }
 
